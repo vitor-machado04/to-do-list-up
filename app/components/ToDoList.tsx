c6a54c511631a498c7c3cede2000b6fefa7ce7b8
Vitor Machado <122799964+vitor-machado04@users.noreply.github.com>
2024-06-27 20:25:29 -0300

    feat: adicionando barra de pesquisa

--- app/components/ToDoList.tsx
+++ app/components/ToDoList.tsx
@@ -10,22 +10,37 @@ interface ToDoListProps {
 
 const ToDoList: React.FC<ToDoListProps> = ({ tasks }) => {
     const [filterDate, setFilterDate] = useState<string>('');
+    const [searchTerm, setSearchTerm] = useState<string>('');
 
-    const openTasks = tasks.filter(task => !task.status && (!filterDate || new Date(task.createdAt).toDateString() === new Date(filterDate).toDateString()));
-    const closedTasks = tasks.filter(task => task.status);
-    
+    const filteredTasks = tasks.filter(task => 
+        (searchTerm === '' || task.description.toLowerCase().includes(searchTerm.toLowerCase())) &&
+        (!filterDate || new Date(task.createdAt).toDateString() === new Date(filterDate).toDateString())
+    );
+
+    const openTasks = filteredTasks.filter(task => !task.status);
+    const closedTasks = filteredTasks.filter(task => task.status);
+
+    const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(e.target.value);
+    };
 
     return (
         <div className="overflow-x-auto">
             <div className="flex justify-between items-center mb-5">
-                <h2 className='font-bold text-xl'>Tarefas Abertas</h2>
                 <input 
-                    type="date" 
-                    className="input input-bordered w-full max-w-xs"
+                    className="input input-bordered w-full max-w-xs" 
+                    placeholder="Pesquisar por tarefa"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                />
+                <input
+                    type="date"
+                    className="input input-bordered"
                     value={filterDate}
                     onChange={e => setFilterDate(e.target.value)}
                 />
             </div>
+            <h2 className='font-bold text-xl'>Tarefas Abertas</h2>
             <table className="table">
                 <tbody className='text-lg'>
                     {openTasks.map(task => <Task key={task._id} task={task} />)}
--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
           <h1 className="text-2xl font-bold">Lista de Tarefas</h1>
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn m-1">
-              Theme
+              Tema
               <svg width="12px" height="12px" className="h-2 w-2 fill-current opacity-60 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048"><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path></svg>
             </div>
             <ul tabIndex={0} className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52">
