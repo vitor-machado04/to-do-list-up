257dc4a8758dac3997296684cdf9609c6df26fbf
Vithugsil <vitorhugosilva012@gmail.com>
2024-06-27 20:23:15 -0300

    date issue fixed

--- app/components/ToDoList.tsx
+++ app/components/ToDoList.tsx
@@ -9,18 +9,29 @@ interface ToDoListProps {
 }
 
 const ToDoList: React.FC<ToDoListProps> = ({ tasks }) => {
+    // const [filterDate, setFilterDate] = useState<string>('');
+
+    // const openTasks = tasks.filter(task => !task.status && (!filterDate || new Date(task.createdAt).toDateString() === new Date(filterDate).toDateString()));
+    // const closedTasks = tasks.filter(task => task.status );
     const [filterDate, setFilterDate] = useState<string>('');
 
-    const openTasks = tasks.filter(task => !task.status && (!filterDate || new Date(task.createdAt).toDateString() === new Date(filterDate).toDateString()));
+    const openTasks = tasks.filter(task => {
+        const taskDate = new Date(task.createdAt).toDateString();
+        const filterDateString = filterDate ? new Date(filterDate).toDateString() : null;
+        return !task.status && (!filterDate || taskDate === filterDateString);
+    });
+
     const closedTasks = tasks.filter(task => task.status);
-    
+
+    console.log(closedTasks);
+    console.log(openTasks);
 
     return (
         <div className="overflow-x-auto">
             <div className="flex justify-between items-center mb-5">
                 <h2 className='font-bold text-xl'>Tarefas Abertas</h2>
-                <input 
-                    type="date" 
+                <input
+                    type="date"
                     className="input input-bordered w-full max-w-xs"
                     value={filterDate}
                     onChange={e => setFilterDate(e.target.value)}
